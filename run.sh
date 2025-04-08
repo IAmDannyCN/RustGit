a49e2a8dd6425760c4bbdeba60b6f28d9993512e
IAmDannyCN <I_Am_Danny@126.com>
2025-04-08 21:57:18 +0800

    update `run.sh`

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-RUST_HOME=~/rust
+if [ -z "$RUST_HOME" ]; then
+    echo "Error: RUST_HOME is not set."
+    exit 1
+fi
 cargo build || exit 1
+if [ ! -d "$RUST_HOME/test" ]; then
+    mkdir -p "$RUST_HOME/test"
+fi
 rm -rf "$RUST_HOME/test/"{.,}* 2>/dev/null
 cp "$RUST_HOME/target/debug/git" "$RUST_HOME/test/"
