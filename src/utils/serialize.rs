127690cc85a207113a102b3b17308f8144e79a16
IAmDannyCN <I_Am_Danny@126.com>
2025-04-10 22:25:47 +0800

    Add serialze and deserialze

---
+++ src/utils/serialize.rs
@@ -0,0 +1,25 @@
+use std::process;
+
+use base64::{engine::general_purpose, Engine as _};
+
+pub fn serialize(text: &str) -> String {
+    general_purpose::STANDARD.encode(text)
+}
+
+pub fn deserialize(text: &str) -> String {
+    match general_purpose::STANDARD.decode(text) {
+        Ok(decoded_bytes) => {
+            match String::from_utf8(decoded_bytes) {
+                Ok(res) => res,
+                Err(e) => {
+                    println!("Deserialize error when converting Vec<u8> to String: {}", e);
+                    process::exit(1)
+                }
+            }
+        }
+        Err(e) => {
+            println!("Deserialize error when decoding base64: {}", e);
+            process::exit(1)
+        }
+    }
+}
